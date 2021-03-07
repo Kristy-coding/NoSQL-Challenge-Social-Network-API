70204293385567c17690af9a6ce6fdd546c990e0
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2021-03-07 08:49:13 -0500

    pseduo coded date format

---
+++ utils/dateFormat.js
@@ -0,0 +1,21 @@
+// require date fns?
+
+//format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+//=> '2014-02-11'
+
+//2021-03-07T12:41:30.778Z
+
+function dateFormat(unformatedDate){
+
+    // take unformated date and split it at "-" so it returns an array with 3 indexes 
+    // const year = array[0], parse into to make it a number
+    //const month = array[1]
+    // make month string into an array of characters if first character is zero, get rid of the zero
+    // then parse int string back to number
+    // subtract 1 from the number to get the monthIndex 
+    // const day = first two characters in array[2], partse int to turn day to a number
+
+
+    // return format(newDate(year, monthIndex, day), 'yyyy-MM-dd')
+
+}
